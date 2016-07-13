d5288b2541b2ac44dab78a4b3c24fdd9ed8437ce
Stephen M. Pallen <smpallen99@yahoo.com>
2016-07-13 16:06:21 -0400

    built first model

---
+++ test/models/post_test.exs
@@ -0,0 +1,18 @@
+defmodule Admin2.PostTest do
+  use Admin2.ModelCase
+
+  alias Admin2.Post
+
+  @valid_attrs %{body: "some content", title: "some content"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = Post.changeset(%Post{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = Post.changeset(%Post{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
---
+++ web/models/post.ex
@@ -0,0 +1,19 @@
+defmodule Admin2.Post do
+  use Admin2.Web, :model
+
+  schema "posts" do
+    field :title, :string
+    field :body, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title, :body])
+    |> validate_required([:title, :body])
+  end
+end
