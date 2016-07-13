4d6e0712945fe0bd6cbabd103f0b7cce2e5af0b6
Stephen M. Pallen <smpallen99@yahoo.com>
2016-07-13 16:04:16 -0400

    removed ex admin dep

--- config/config.exs
+++ config/config.exs
@@ -26,9 +26,3 @@ config :logger, :console,
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
 
-  config :ex_admin,
-    repo: Admin2.Repo,
-    module: Admin2,
-    modules: [
-      # Admin2.ExAdmin.Dashboard,
-    ]
